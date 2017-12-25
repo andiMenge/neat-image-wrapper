1ebfc1da4f342ccc69b8e626c54105456896cbf2
Andreas Menge <andi.menge@gmail.com>
2017-12-25 17:36:22 +0100

    add build script

--- build-release-binary.sh
+++ build-release-binary.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 name="neatbatch"
+set -ex
 
-printf "\n## BUILD RELEASES ##"
+printf "\n## BUILD RELEASES ##\n"
 
 echo " build macOS binary..."
-GOOS=darwin go build -o $name main.go
+GOOS=darwin go build -o $name ./main.go
 
 echo "build windows binary..."
-GOOS=windows go build -o $name main.go
+GOOS=windows go build -o $name.exe ./main.go
